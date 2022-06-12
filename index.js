51d7961e38c52c918e334637e84c6cfcdb2b211e
moses ngige <moses32ngigz@gmail.com>
2022-06-12 11:12:26 +0300

    commit tree 265e6ee21a76fe5aa26c60226ec2b86c5936278b

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ function receivesAFunction (callback) {
 
 function returnsANamedFunction(){
     return function named(){
+        console.log("moz")
 
     };
 }
 function returnsAnAnonymousFunction (){
-    return function () {}; 
+    return function () {
+        console.log("moz")
+    }; 
 }
